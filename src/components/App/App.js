709c12ce1c9c163473498aebb2f47c1a60faecf5
sam-starikov <sam_starikov@rambler.ru>
2024-03-22 16:37:26 +0400

    add fonts

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 style={{ fontFamily: 'Gotham', fontWeight: 700 }}>Font weight</h1>
+      <h1 style={{ fontFamily: 'Gotham pro', fontWeight: 700 }}>Font weight</h1>
+    </>
+  )
+}
+
+export default App
